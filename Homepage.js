16f7a3a51e2a1c3a7867104699f8ad38d154167a
edisonarexha <75527154+edisonarexha@users.noreply.github.com>
2021-02-10 16:48:13 +0100

    Add files via upload

---
+++ Homepage.js
@@ -0,0 +1,35 @@
+/*---------------------------Per Navbar--------------------------*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+     
+          // Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+
+
+    
