3c3db514153fb3b9f51775da7c8bdc4ae98795fc
erestinabytyqi <eb49282@ubt-uni.net>
2021-02-14 15:40:46 +0100

    news database

--- Admin/admin.php
+++ Admin/admin.php
@@ -1,3 +1,7 @@
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,8 +41,29 @@
 				</div>
 					
 				<div id="other-tab">
-					
-					<h1>Add News</h1>
+					<div style="border: 1px solid white;width: 60%;height: 100%;margin-left: 20%">
+					<div style="border: 1px solid white;width: 100%;height:10%;float: left;display: inline;">
+						<h1 style="color: white;text-align: center;font-family: 'Montserrat', sans-serif;">
+							What is the News for today Admin?
+						</h1>
+					</div>
+					<div style="border:1px solid white; width: 100%;height:7%;float: left;display: inline">
+						<h2 style="color: white;font-family: 'Montserrat', sans-serif;margin-left: 10%">
+							Updates for today:
+						</h2>
+					</div>
+					<div style="border: 1px solid white;width: 100%;height: 83%;float: left;display: inline">
+						<form method="POST" id="form" action="../News/insertation.php"  enctype="multipart/form-data">
+							<input type="text" name="titulli" placeholder="Titulli" style="margin-top: 4%;margin-left: 3%;padding: 10px 60px;color: white;background-color: black;border-color: white;border-top: transparent;border-left: transparent;border-right: transparent;font-size: 20px" id="titulli">
+							<input type="text" name="data" placeholder="Date & Time" style="margin-top: 4%;margin-left: 20%;padding: 10px 60px;color: white;background-color: black;border-color: white;border-top: transparent;border-left: transparent;border-right: transparent;font-size: 20px" id="data">
+							<input type="file" name="fotografi" placeholder="Choose File" style="color: white;background-color: black;padding: 10px 20px; margin-left: 5%;margin-top: 10%" id="fotografi">
+							<br>
+							<textarea name="mesazhi" rows="14" cols="120" style="margin-left: 5%;margin-top: 3%;background-color: black;color: white;border: 2px solid white;resize: none;" placeholder="Type Your news here" id="Mesazhi"></textarea>
+							<button type="submit" style="padding: 8px 60px;margin-left:80%;margin-top: 3%;background-color: black;border: 2px solid white;color :white;" action="insertation.php">Post</button>
+						</form>
+					</div>
+
+			</div>
 				</div>
 
 				<div id="trend-tab"> Users-messages</div>
--- News/hees.php
+++ News/hees.php
@@ -1,11 +1,29 @@
- <?php
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "hees";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if(!$conn){
-	die("Deshtoi lidhja".mysqli_connect_error());
-}
-?>
 
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create database
+//$sql = "CREATE DATABASE lajmedb";
+//if ($conn->query($sql) === TRUE) {
+ // echo "Database created successfully";
+//} else {
+ // echo "Error creating database: " . $conn->error;
+//}
+
+
+
+$conn->close();
+
+
+?>
