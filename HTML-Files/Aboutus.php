f48dd1fad4707af865726d648fede383bb4cc308
erestinabytyqi <eb49282@ubt-uni.net>
2021-02-24 17:58:26 +0100

    init changes sql

--- HTML-Files/Aboutus.php
+++ HTML-Files/Aboutus.php
@@ -64,14 +64,36 @@
             <h3 id="textcolor">Kosovo 20/2021</h3>
         </div>
         <div id="lorem">
-<p id="te">It is a long established fact that a reader >
-will be distracted by the readable content 
-of a page when looking at its layout. The 
-point of using Lorem Ipsum is that it has
- a more-or-less normal distribution of letters,
-  as opposed to using 'Content here, content here',
-   making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p>
-        </div>
+<p id="te">The charity was founded in 1950 as HEES Inc. by Robert Pierce and co-founder Frank Phillips with the first 
+office in Portland, Oregon. Originally, the charity operated as a missionary service organization
+ meeting emergency needs in crisis areas in East Asia, where in 1954 it opened an office in South Korea.
+  In order to restructure, the organization HEESInternational was founded in 1977 by Walter Stanley Mooneyham the then president of HEES.
+In 1967, the Mission Advanced Research and Communication Center (MARC) was founded 
+by Ed Dayton as a division of HEESInternational. It became the organizational backbone 
+of the Lausanne Committee for World Evangelization, collected and published data about "unreached people" and also published
+ the "Mission Handbook: North American Protestant Ministries Overseas".
+During the 1970s, HEES began training families to build small farms
+ by teaching agricultural skills aiming to make lasting effects in 
+ the communities they were helping by promoting self-reliance
+  The organization also began installing water pumps for clean
+   water in communities which caused infant mortality rates to drop. 
+   Volunteers now use the fresh water to teach communities gardening and irrigation and promote good health.
+
+During the 1990s, HEES International began focusing on the needs 
+of children who had been orphaned in Uganda, Romania, and Somalia 
+in response to AIDS, neglect, and civil war, respectively. HEES
+ began working with communities, health providers, faith-based 
+ organisations and people living with HIV and AIDS to encourage 
+ an end to stigmatisation, better understanding of HIV prevention 
+ and community care for those living with AIDS, and orphans left 
+ behind by the pandemic. They also joined the United Nations peacekeeping
+  efforts to help those affected by civil war. HEESalso started to openly 
+  promote the international ban on land mines. In 1994 HEESUS moved to Washington State.
+
+According to Forbes magazine, as of December 2014, HEES is the 11th 
+largest charity in the United States with total revenue of over 981 million dollars.
+
+In 2020, it worked in more than 90 countries and had 37,000 employees.</div>
     </div>
     <div id="aboutustext1">
         <p id="te">It is a long established fact that a reader >
